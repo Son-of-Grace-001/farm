2aab51a344f8102f5380c59fb633ec1163d46664
Son-of-Grace-001 <horlharmighty2000@gmail.com>
2023-07-24 05:49:15 -0700

    removed Django version

--- project/settings.py
+++ project/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get(('secret_key'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('debug',cast=bool)
+DEBUG = os.environ.get('debug')
 
 ALLOWED_HOSTS = ['*']
 
