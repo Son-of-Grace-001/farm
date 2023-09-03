eac014d13bb97ea664b33ed7a97317cf6decf00a
Son-of-Grace-001 <horlharmighty2000@gmail.com>
2023-09-03 15:49:47 -0700

    user model

--- App/views.py
+++ App/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import auth, get_user_model
+from django.contrib.auth.models import auth
+from django.contrib.auth import get_user_model
 User = get_user_model
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
